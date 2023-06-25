5f2f7615c6f323ea32461b2e1a71ac9317220357
Erfan Farahbakhsh <itserfan.farahbakhsh@gmail.com>
2023-06-25 21:35:39 +0330

    Create Custom Container Class

--- lib/main.dart
+++ lib/main.dart
@@ -5,28 +5,35 @@ void main() {
     MaterialApp(
       home: Scaffold(
         // backgroundColor: Color.fromARGB(255, 102, 18, 247),
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-              colors: [
-                Color.fromARGB(255, 28, 6, 107),
-                Color.fromARGB(255, 79, 15, 116),
-              ],
-            ),
-          ),
-          child: const Center(
-            child: Text(
-              'Hello Erfan',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 28,
-              ),
-            ),
-          ),
-        ),
+        body: GradientContainer(),
       ),
     ),
   );
+}
+
+class GradientContainer extends StatelessWidget {
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+          colors: [
+            Color.fromARGB(255, 28, 6, 107),
+            Color.fromARGB(255, 79, 15, 116),
+          ],
+        ),
+      ),
+      child: const Center(
+        child: Text(
+          'Hello Erfan',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 28,
+          ),
+        ),
+      ),
+    );
+  }
 }
