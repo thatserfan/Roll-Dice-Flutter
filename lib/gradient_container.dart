3f38087e2ecaafe7843f321ee119a05e1ab13be4
Erfan Farahbakhsh <itserfan.farahbakhsh@gmail.com>
2023-06-26 16:35:40 +0330

    Make styled_text.dart Dynamically

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:roll_dice_app/styled_text.dart';
 
+const startAlignment = Alignment.topLeft;
+const endAlignment = Alignment.bottomRight;
+
 class GradientContainer extends StatelessWidget {
   const GradientContainer({super.key});
 
@@ -9,8 +12,8 @@ class GradientContainer extends StatelessWidget {
     return Container(
       decoration: const BoxDecoration(
         gradient: LinearGradient(
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
+          begin: startAlignment,
+          end: endAlignment,
           colors: [
             Color.fromARGB(255, 28, 6, 107),
             Color.fromARGB(255, 79, 15, 116),
@@ -18,7 +21,7 @@ class GradientContainer extends StatelessWidget {
         ),
       ),
       child: const Center(
-        child: StyledText(),
+        child: StyledText("Hello Erfan"),
       ),
     );
   }
